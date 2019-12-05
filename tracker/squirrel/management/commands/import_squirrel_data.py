9424402faf535faa9f58fb0643bfd4b8a72edc32
Xinyi Shu <xs2321@columbia.edu>
2019-12-05 17:07:50 +0000

    Delete Duplicates and finish export func

--- tracker/squirrel/management/commands/import_squirrel_data.py
+++ tracker/squirrel/management/commands/import_squirrel_data.py
@@ -19,35 +19,40 @@ class Command(BaseCommand):
         with open(path,'r') as f:
             reader = csv.reader(f)
             next(reader)
+            idlist=list()
             for row in reader:
-                date = row[5]
-                m = date[:2]
-                d = date[2:4]
-                y = date[4:]
-                new_date = y + '-' + m + '-' + d
-                p = Squirrel(
-                    lon = float(row[0]),
-                    lat = float(row[1]),
-                    squirrel_id = row[2],
-                    shift = row[4],
-                    date = new_date,
-                    age = row[7],
-                    pri_color = row[8],
-                    location = row[12],
-                    specific_location = row[14],
-                    running = self.str_to_bool(row[15]),
-                    chasing = self.str_to_bool(row[16]),
-                    climbing = self.str_to_bool(row[17]),
-                    eating = self.str_to_bool(row[18]),
-                    foraging = self.str_to_bool(row[19]),
-                    other_activities = row[20],
-                    kuks = self.str_to_bool(row[21]),
-                    quaas = self.str_to_bool(row[22]),
-                    moans = self.str_to_bool(row[23]),
-                    tail_flags = self.str_to_bool(row[24]),
-                    tail_twitches = self.str_to_bool(row[25]),
-                    approaches = self.str_to_bool(row[26]),
-                    indifferent = self.str_to_bool(row[27]),
-                    runs_from = self.str_to_bool(row[28]),
-                )
-                p.save()
+                if row[2] in idlist:
+                    pass
+                else:
+                    idlist.append(row[2])
+                    date = row[5]
+                    m = date[:2]
+                    d = date[2:4]
+                    y = date[4:]
+                    new_date = y + '-' + m + '-' + d
+                    p = Squirrel(
+                        lon = float(row[0]),
+                        lat = float(row[1]),
+                        squirrel_id = row[2],
+                        shift = row[4],
+                        date = new_date,
+                        age = row[7],
+                        pri_color = row[8],
+                        location = row[12],
+                        specific_location = row[14],
+                        running = self.str_to_bool(row[15]),
+                        chasing = self.str_to_bool(row[16]),
+                        climbing = self.str_to_bool(row[17]),
+                        eating = self.str_to_bool(row[18]),
+                        foraging = self.str_to_bool(row[19]),
+                        other_activities = row[20],
+                        kuks = self.str_to_bool(row[21]),
+                        quaas = self.str_to_bool(row[22]),
+                        moans = self.str_to_bool(row[23]),
+                        tail_flags = self.str_to_bool(row[24]),
+                        tail_twitches = self.str_to_bool(row[25]),
+                        approaches = self.str_to_bool(row[26]),
+                        indifferent = self.str_to_bool(row[27]),
+                        runs_from = self.str_to_bool(row[28]),
+                    )
+                    p.save()
--- tracker/squirrel/urls.py
+++ tracker/squirrel/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
         path('map/', views.all_sightings),
         path('sightings/',views.all_squirrels),
         path('sightings/add/',views.add,name='add'),
-        path('sightings/<squirrel_id>/',views.update,name='update'),
         path('sightings/stats/',views.stats,name='stats'),
+        path('sightings/<squirrel_id>/',views.update,name='update'),
 ]

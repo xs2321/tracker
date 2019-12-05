6804bc7ee5cb2e44b76f7cc043f85f1b7d8c5962
Xinyi Shu <xs2321@columbia.edu>
2019-12-05 15:24:17 +0000

    Stats

--- tracker/squirrel/urls.py
+++ tracker/squirrel/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
         path('map/', views.all_sightings),
         path('sightings/',views.all_squirrels),
-        path('sightings/add/',views.add),
-        path('sightings/<squirrel_id>/',views.update),
+        path('sightings/add/',views.add,name='add'),
+        path('sightings/<squirrel_id>/',views.update,name='update'),
+        path('sightings/stats/',views.stats,name='stats'),
 ]
--- tracker/squirrel/views.py
+++ tracker/squirrel/views.py
@@ -2,6 +2,7 @@ from .models import Squirrel
 from django.shortcuts import render 
 from django.shortcuts import redirect
 from .forms import SquirrelForm
+from django.db import connection
 
 def all_sightings(request):
     sightings = Squirrel.objects.all()
@@ -40,3 +41,16 @@ def update(request, squirrel_id):
     else:
         form = SquirrelForm(instance=squirrel)
     return render(request,'squirrel/edit.html',{'form':form})
+
+def stats(request):
+    cursor = connection.cursor()
+    query = """select age,count(*)
+               from squirrel_squirrel
+               group by age"""
+    cursor.execute(query)
+    result = cursor.fetchall()
+    dict_1=dict()
+    for i in range(len(result)):
+        dict_1[result[i][0]]=result[i][1]
+    return render(request,'squirrel/stats.html',locals())
+
